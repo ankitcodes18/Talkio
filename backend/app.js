18c5254cc3b9299cd00067cb677d99a0767f0845
ankitcodes18 <kankit43970@gmail.com>
2025-12-06 11:32:57 +0530

    again update cors

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cookieParser()); 
 app.use(cors({
-  origin: "*", 
+  origin: ["http://localhost:5173", "https://talkio-phi.vercel.app"], 
   credentials: true,               
 }));
 app.get('/' , (req , res)=>{
